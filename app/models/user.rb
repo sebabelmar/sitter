950d6fd23680b8655aa98cd0a74e0200ea039d0d
sebabelmar <si.belmar@gmail.com>
2014-09-06 22:14:11 -0700

    updates user model: email and password validation and has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
 
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+  has_secure_password
+
+  validates_presence_of :password, :email
 end
