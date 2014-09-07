a6a6918cf9e0c4f835b2705fb0e4893e939ffb98
sebabelmar <si.belmar@gmail.com>
2014-09-06 22:13:08 -0700

    add session controller and update users controller with sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,35 @@
 class UsersController < ApplicationController
+  def index
+    puts "WE ARE HERE"
+  end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      flash[:notice] = "User couldn't be saved. Please try again."
+      redirect_to new_user_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    flash[:notice] = "#{@user.first_name} #{@user.last_name} has been deleted."
+    @user.destroy
+  end
+
+  def home
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :family_name, :email, :password, :phone_number_one, :phone_number_two, :zip_code)
+  end
 end
